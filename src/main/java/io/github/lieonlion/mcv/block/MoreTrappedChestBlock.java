9ac3f547b7c627c25369705b10464635b73f39dc
lieonlion <lieonlion4@gmail.com>
2024-06-18 16:45:13 +0100

    1.5.5

---
+++ src/main/java/io/github/lieonlion/mcv/block/MoreTrappedChestBlock.java
@@ -0,0 +1,56 @@
+package io.github.lieonlion.mcv.block;
+
+import io.github.lieonlion.mcv.block.entity.MoreTrappedChestBlockEntity;
+import io.github.lieonlion.mcv.init.McvBlockInit;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.stats.Stat;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.Mth;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.ChestBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.MapColor;
+
+public class MoreTrappedChestBlock extends MoreChestBlock {
+    public final String chestType;
+
+    public MoreTrappedChestBlock(MapColor colour, String chestType) {
+        super(Properties.ofFullCopy(Blocks.CHEST).mapColor(colour), () -> McvBlockInit.MORE_TRAPPED_CHEST_BLOCK_ENTITY, chestType);
+        this.chestType = chestType;
+
+        registerMaterialNameRetriever();
+    }
+
+    public MoreTrappedChestBlock(MapColor colour, SoundType sound, String chestType) {
+        super(Properties.ofFullCopy(Blocks.CHEST).mapColor(colour).sound(sound), () -> McvBlockInit.MORE_TRAPPED_CHEST_BLOCK_ENTITY, chestType);
+        this.chestType = chestType;
+
+        registerMaterialNameRetriever();
+    }
+
+    protected Stat<ResourceLocation> getOpenChestStat() {
+        return Stats.CUSTOM.get(Stats.TRIGGER_TRAPPED_CHEST);
+    }
+
+    public boolean isSignalSource(BlockState p_57587_) {
+        return true;
+    }
+
+    public int getSignal(BlockState state, BlockGetter getter, BlockPos pos, Direction direction) {
+        return Mth.clamp(ChestBlockEntity.getOpenCount(getter, pos), 0, 15);
+    }
+
+    public int getDirectSignal(BlockState state, BlockGetter getter, BlockPos pos, Direction direction) {
+        return direction == Direction.UP ? state.getSignal(getter, pos, direction) : 0;
+    }
+
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new MoreTrappedChestBlockEntity(pos, state);
+    }
+}
---
+++ src/main/java/io/github/lieonlion/mcv/mixin/CreativeModeTabsMixin.java
@@ -0,0 +1,18 @@
+package io.github.lieonlion.mcv.mixin;
+
+import io.github.lieonlion.mcv.init.McvBlockInit;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import java.util.function.Supplier;
+
+@Mixin(CreativeModeTabs.class)
+public abstract class CreativeModeTabsMixin {
+    @ModifyArg(method = "bootstrap", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/CreativeModeTab$Builder;icon(Ljava/util/function/Supplier;)Lnet/minecraft/world/item/CreativeModeTab$Builder;", ordinal = 13))
+    private static Supplier<ItemStack> applyChestTabIcon(Supplier<ItemStack> iconSupplier) {
+        return () -> new ItemStack(McvBlockInit.OAK_CHEST);
+    }
+}
